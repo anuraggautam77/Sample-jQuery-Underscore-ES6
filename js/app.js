a4066b3482ff63a8c5464cb6c9d76b389ea6b89e
Anurag <anuraggautam77@gmail.com>
2017-05-28 20:56:27 +0530

    Add project structure

---
+++ js/app.js
@@ -0,0 +1,28 @@
+/**
+ * Created by anurag on 18-02-2017.
+ */
+
+ class App {
+    constructor (){
+        this.jsonPath = "assets/cart.json";
+     }
+  invokeApp() {
+      this.getjsonData();
+
+   }
+
+  getjsonData() {
+
+      window.utils.callTofile(this.jsonPath, (data)=>{
+      window.utils.setJsonData(data);
+         var cart = new CartApp(),  promo = new PromoCode(), discount = new Discount(),  estimatedtotal = new EstimatedTotal();
+         cart.init(); promo.init();  discount.init();   estimatedtotal.init();
+  });
+
+}
+
+
+}
+
+var app = new App();
+app.invokeApp();
